c0100a8700666ec0675c38cb7c118c3c2e1b2a4a
karikai <kari.k.abraham@gmail.com>
2019-08-01 15:16:22 -0400

    User Auth, Personal Files, Download Link

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,61 +5,94 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\DatabaseConnector;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DashboardController extends AbstractController
 {
     public function download(DatabaseConnector $db, LoggerInterface $logger)
     {
+        session_start();
+        $user_id = $_SESSION['user_id'];
+                
         $conn = $db->sqlConnection();
-        $sql = "SELECT * FROM files";
+        $sql = "SELECT * FROM files WHERE uploaded_by = $user_id";
         $result = mysqli_query($conn, $sql);
-
+        $logger->info(mysqli_num_rows($result));
         $usersArray = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
         
-        return $this->render('dashboard/download.html.twig', [
-            'dataArray' => $usersArray
-        ]);
+        if (mysqli_num_rows($result) !== 0) {
+            return $this->render('dashboard/download.html.twig', [
+                'dataArray' => $usersArray,
+                'user_id' => $_SESSION['user_id']
+            ]);
+        } else {
+            return $this->render('dashboard/download-no-uploads.html.twig');
+        }
     }
     
     public function upload(DatabaseConnector $db, LoggerInterface $logger)
     {
         session_start();
-
-        $conn = $db->sqlConnection();
-
-        $statusMsg = '';
+        $user_id = $_SESSION['user_id'];
         
+        $conn = $db->sqlConnection();
+        $statusMsg = '';
         if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
             // File upload path
+            $timestamp = time(); 
             $targetDir = getcwd() . "/uploads/";
-            $fileName = basename($_FILES["file"]["name"]);
+            $fileType = pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION);
+            $logger->info($fileType);
+            $fileName = basename($_FILES["file"]["name"], ".$fileType");
+            $logger->info($fileName);
+            $fileName =  $fileName . '-' . $user_id . $timestamp . ".$fileType";
             $targetFilePath = $targetDir . $fileName;
             $logger->info($targetFilePath);
-            $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
-            // Allow certain file formats
-            $allowTypes = array('jpg','png','jpeg','gif','pdf');
-            if(in_array($fileType, $allowTypes)){
-                // Upload file to server
-                if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
-                    // Insert image file name into database
-                    $sql = "INSERT into files (file_name, uploaded_on) VALUES ('".$fileName."', NOW())";
-                    $result = mysqli_query($conn, $sql);
-                    if($result){
-                        $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
-                    }else{
-                        $statusMsg = "File upload failed, please try again.";
-                    } 
-                }else{
-                    $statusMsg = "Sorry, there was an error uploading your file.";
+            $logger->info($_FILES["file"]["tmp_name"]);
+            $logger->info($_FILES["file"]["name"]);
+            $logger->info($fileName);
+            // Upload file to server
+            if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+                // Insert image file name into database
+                $sql = "INSERT into files (file_name, uploaded_by, uploaded_on) VALUES ('" . $fileName . "', $user_id, NOW())";
+                $result = mysqli_query($conn, $sql);
+                if ($result) {
+                    $statusMsg = "The file " . $fileName . " has been uploaded successfully.";
+                    $logger->info($statusMsg);
+                    return $this->redirectToRoute('download');
+                } else {
+                    $statusMsg = "File upload failed, please try again.";
+                    $logger->info($statusMsg);
                 }
-            }else{
-                $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+            } else {
+                $statusMsg = "Sorry, there was an error uploading your file.";
+                $logger->info($statusMsg);
             }
         }else{
             $statusMsg = 'Please select a file to upload.';
+            $logger->info($statusMsg);
         }
-
+        
         return $this->render('dashboard/upload.html.twig');
     }
+    
+    public function downloadLink(DatabaseConnector $db, LoggerInterface $logger, $uid, $filename)
+    {
+        session_start();
+        $user_id = $_SESSION['user_id'];
+        $targetDir = getcwd() . "/uploads/";
+        
+        if ($user_id === $uid) {
+            $response = new BinaryFileResponse($targetDir.$filename);
+            $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT);
+            return $response;
+        } else {
+            return new Response('File Not Found');
+        }
+            
+    }
+    
 }
--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,8 +5,7 @@ namespace App\Controller;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\DatabaseConnector;
-use Symfony\Component\Routing\Annotation\Route;
-use mysqli;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ProfileController extends AbstractController
 {
@@ -15,44 +14,93 @@ class ProfileController extends AbstractController
         session_start();
 
         $conn = $db->sqlConnection();
-        
-        if(isset($_POST['username'])) {
+
+        if (isset($_POST['username'])) {
             $username = $_POST['username'];
             $password = $_POST['password'];
-            $logger->info('username => ' . $username);
-            $logger->info('password => ' . $password);
-            if($username !== '' and $password !== '') {
+            if ($username !== '' and $password !== '') {
                 $sql = "SELECT * FROM users WHERE username = '$username'";
                 $result = mysqli_query($conn, $sql);
                 $numberOfRows = mysqli_num_rows($result);
-                
-                if($numberOfRows === 1) {
+
+                if ($numberOfRows === 1) {
                     $user = mysqli_fetch_assoc($result);
                     $passwordHash = hash('sha256', $password);
-                    $logger->info($passwordHash);
-                    $logger->info($user['pwd']);
                     if ($passwordHash === $user['pwd']) {
                         $_SESSION["user_id"] = $user['id'];
                         $_SESSION["user_email"] = $user['email'];
                         $_SESSION["user_username"] = $user['username'];
                         $_SESSION["user_creation_date"] = $user['created_on'];
-                        return $this->render('dashboard/download.html.twig');
+                        return $this->redirectToRoute('download');
                     } else {
-                        return $this->render('profile/index.html.twig');
+                        return $this->redirectToRoute('index');
                     }
                 } else {
                     // No Such User Exists
+                    return $this->render('profile/index.html.twig');
                 }
             }
         } else {
-            return $this->render('profile/index.html.twig', [
-            ]);
+            return $this->render('profile/index.html.twig');
         }
-
     }
-    
-    public function createProfile()
+
+    public function createProfile(DatabaseConnector $db, LoggerInterface $logger)
     {
-        return $this->render('profile/sign-up.html.twig');
+        session_start();
+
+        if (isset($_SESSION['user_id'])) {
+            return $this->redirectToRoute('download');
+        } else {
+            $conn = $db->sqlConnection();
+            if (isset($_POST['new-username'])) {
+                $username = $_POST['new-username'];
+                $password = $_POST['new-password'];
+                $confirm_password = $_POST['new-confirm-password'];
+                $email = $_POST['new-email'];
+                if ($username !== '' and $password !== '' and $confirm_password !== '' and $email !== '') {
+                    $sql = "SELECT * FROM users WHERE username = '$username'";
+                    $result = mysqli_query($conn, $sql);
+                    $numberOfRows = mysqli_num_rows($result);
+
+                    if ($numberOfRows === 0) {
+                        $passwordHash = hash('sha256', $password);
+                        $new_sql = "INSERT INTO users (email, username, pwd, created_on) VALUES ('$email', '$username', '$passwordHash', CURRENT_TIMESTAMP)";
+                        $new_result = mysqli_query($conn, $new_sql);
+                        
+                        if($new_result) {
+                            $logger->info($new_result);
+                            $userdata_sql = "SELECT * FROM users WHERE username = '$username'";
+                            $userdata_result = mysqli_query($conn, $userdata_sql);
+                            $user = mysqli_fetch_assoc($userdata_result);
+                            $_SESSION["user_id"] = $user['id'];
+                            $_SESSION["user_email"] = $user['email'];
+                            $_SESSION["user_username"] = $user['username'];
+                            $_SESSION["user_creation_date"] = $user['created_on'];
+                            return $this->redirectToRoute('download');
+                        } else {
+                            return $this->render('profile/sign-up.html.twig');
+                        }
+                        
+                    } else {
+                        $logger->info('Username already exist');
+                        return $this->render('profile/index.html.twig');
+                    }
+                } else {
+                    return $this->render('profile/sign-up.html.twig');
+                }
+            } else {
+                return $this->render('profile/sign-up.html.twig');
+            }
+        }
+    }
+
+    public function logOut()
+    {
+        session_start();
+        session_destroy();
+
+        // $url = $this->generateUrl('', ['max' => 10]);
+        return $this->redirectToRoute('index');
     }
 }
